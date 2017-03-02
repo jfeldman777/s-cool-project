305140a00f123e3819a77dc0ed437c9efc293030
jfeldman777 <jfeldman777@gmail.com>
2017-03-02 17:58:35 +0300

    Russian welcome, local try

--- sky/settings.py
+++ sky/settings.py
@@ -114,12 +114,14 @@ AUTH_USER_EMAIL_UNIQUE = True
 #DEFAULT_FROM_EMAIL = 'info@google.ru'
 
 #import os
-EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
-EMAIL_HOST= 'smtp.sendgrid.net'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
-
+try:
+    EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
+    EMAIL_HOST= 'smtp.sendgrid.net'
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
+except:
+    pass
 
 #LOGIN_REDIRECT_URL = 'views.home'
 # Internationalization
